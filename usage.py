87953f0b56748e40e1aec6765ddc1450b9c4f059
James Rigassio <james.rigassio@gmail.com>
2017-12-20 00:08:38 -0500

    login implemented successfully

--- usage.py
+++ usage.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from tabulate import tabulate
+import re
 # Raw data courtesy of Allen Guo: https://github.com/guoguo12/hivemind
 secret_sauce = 'https://labs.aguo.us/hivemind/data/latest.json'
 
@@ -14,8 +14,19 @@ data = json_object['data']
 # print data['derby.cs']
 
 #returns a dict or 2d list to be turned into a table
-def result():
-    return data['derby.cs']
+def table():
+    usage_matrix = []
+    server_list = [x for x in data.keys() if data[x] != {}]
+    return server_list
 
-def least_busy():
-    return 'derby.cs'
+def least_busy(hive_only=True):
+    usage = {}
+    if hive_only:
+        server_list = [x for x in data.keys() if data[x] != {} and
+        not re.match("s[0-9]*-[0-9]*", x) and not re.match("hpse", x)]
+    else:
+        server_list = [x for x in data.keys() if data[x] != {}]
+    for s in server_list:
+        pct = data[s]['load_avgs'][1]
+        usage[pct] = s
+    return usage[sorted(usage)[0]]
